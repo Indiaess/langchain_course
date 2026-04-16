adba022215a414fbb4b23bd7ed0e8ba2c6651b4c
Indiaess <satyam.tiwari@essindia.com>
2026-04-16 16:06:43 +0530

    Basic Search Agent

--- main.py
+++ main.py
@@ -1,43 +1,41 @@
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+from tavily import TavilyClient
+
+
 load_dotenv()
 
-
 openai_api_key = os.getenv("OPENAI_API_KEY")
+tavily_api_key = os.getenv("TAVILY_API_KEY")
+
+tavily = TavilyClient(api_key=tavily_api_key)  
+
+@tool
+def search(query: str) -> str:
+    """Search the web for information"""
+    print(f"Searching for {query}")
+
+    response = tavily.search(query=query,search_depth="advanced")
+    return response["results"][0]["content"]
+
+
+llm = ChatOpenAI(model = "gpt-4o", temperature = 0)
+
+tools = [search]
+
+agent = create_agent(model = llm, tools = tools)
+
 
 def main():
-    
-    information = """
-    Elon Musk is a well-known entrepreneur, 
-    inventor, and business magnate. 
-    He was born on June 28, 1971, in South Africa. 
-    Musk is the CEO of Tesla, where he leads the development of electric cars and clean energy solutions,
-    and also the founder of SpaceX, which focuses on space exploration and reusable rockets. He has also been involved in companies like Neuralink and The Boring Company. 
-    Musk is known for his ambitious vision to colonize Mars, reduce global warming, and advance technology for the future."""
-
-    summary_template = """
-    Given the following information, provide a concise summary of the text.
-    
-    Information: {information}
-    
-    Summary: """
-
-    prompt_template = PromptTemplate(
-        input_variables=["information"],
-        template=summary_template
-    )
-
-    llm = ChatOpenAI(temperature= 0, model = "gpt-4o", api_key=openai_api_key)
-
-    chain = prompt_template | llm
-
-    response = chain.invoke({"information": information})
-
-    print(response.content)
-
+    response = agent.invoke({"messages": [{"role": "user", "content": "Who won the IPL match on 15th April 2026 between LSG and RCB? And from how many runs? Who batted first and what was the score of LSG in powerplay"}]})
+    print(response["messages"][-1].content)
 
 if __name__ == "__main__":
     main()
+
+
+ 
